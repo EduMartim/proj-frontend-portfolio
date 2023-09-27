a8db2111fb640cf9d6cc288058cbcfce0fb32a6c
edumartim <edumartim@gmail.com>
2023-09-27 17:37:26 -0300

    refactoring code to get data dynamically

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://github.com/EduMartim/proj-frontend-portifolio/blob/main/data/profile.json';
     const fechting = await fetch(url);
     return await fechting.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('.profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
+
+
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const position = document.getElementById('profile.position');
+    position.innerText = profileData.position;
+
+    const local = document.getElementById('profile.local');
+    local.innerText = profileData.local;
+
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone;
+    phone.href = `tel:${profileData.phone}`;
+
+    const mail = document.getElementById('profile.mail');
+    mail.innerText = profileData.mail;
+    mail.href = `mailto:${profileData.mail}`;
+}
+
+
 (async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
 })();
