74e7d0f4bf8810fb39dbc194ee748954a9675201
edumartim <edumartim@gmail.com>
2023-09-28 10:48:46 -0300

    Adjusting softSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,35 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo');
-    photo.src = profileData.photo;
-    photo.alt = profileData.name;
+  const photo = document.getElementById("profile.photo");
+  photo.src = profileData.photo;
+  photo.alt = profileData.name;
 
-    const name = document.getElementById('profile.name');
-    name.innerText = profileData.name;
+  const name = document.getElementById("profile.name");
+  name.innerText = profileData.name;
 
-    const position = document.getElementById('profile.position');
-    position.innerText = profileData.position;
+  const position = document.getElementById("profile.position");
+  position.innerText = profileData.position;
 
-    const local = document.getElementById('profile.local');
-    local.innerText = profileData.local;
+  const local = document.getElementById("profile.local");
+  local.innerText = profileData.local;
 
-    const phone = document.getElementById('profile.phone');
-    phone.innerText = profileData.phone;
-    phone.href = `tel:${profileData.phone}`;
+  const phone = document.getElementById("profile.phone");
+  phone.innerText = profileData.phone;
+  phone.href = `tel:${profileData.phone}`;
 
-    const mail = document.getElementById('profile.mail');
-    mail.innerText = profileData.mail;
-    mail.href = `mailto:${profileData.mail}`;
+  const mail = document.getElementById("profile.mail");
+  mail.innerText = profileData.mail;
+  mail.href = `mailto:${profileData.mail}`;
+}
+
+function updateSoftSkills(profileData) {
+  const soft = document.getElementById("skills.hard.soft");
+  soft.innerHTML = skills.hard.soft
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
 }
 
 (async () => {
-    const profileData = await fetchProfileData();
-    updateProfileInfo(profileData);
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
+  updateSoftSkills(profileData);
 })();
-
